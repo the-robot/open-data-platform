0afb90e52926c5fb381244d9aa61f469d1db56a1
ghost <9334746+the-robot@users.noreply.github.com>
2023-10-07 01:26:03 +0800

    feat: enable spring scheduling

--- src/main/java/com/odp/opendataplatform/OpenDataPlatformApplication.java
+++ src/main/java/com/odp/opendataplatform/OpenDataPlatformApplication.java
@@ -2,8 +2,10 @@ package com.odp.opendataplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OpenDataPlatformApplication {
 
     public static void main(String[] args) {
