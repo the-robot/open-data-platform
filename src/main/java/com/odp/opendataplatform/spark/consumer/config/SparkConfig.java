4817b3fd293f67270964f632ad483ffe72ac8331
ghost <9334746+the-robot@users.noreply.github.com>
2023-10-07 02:37:00 +0800

    feat: setup spark dependency

---
+++ src/main/java/com/odp/opendataplatform/spark/consumer/config/SparkConfig.java
@@ -0,0 +1,23 @@
+package com.odp.opendataplatform.spark.consumer.config;
+
+import org.apache.spark.sql.SparkSession;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SparkConfig {
+    @Value("${spark.master}")
+    private String sparkMaster;
+
+    @Value("${spark.app.name}")
+    private String appName;
+
+    @Bean
+    public SparkSession sparkSession() {
+        return SparkSession.builder()
+                .appName(appName)
+                .master(sparkMaster)
+                .getOrCreate();
+    }
+}
